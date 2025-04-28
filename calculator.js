491ae4bcebae7e1675b0f3720b15f898d76a18fc
mahnoorhusain <mahnooorhusain@gmail.com>
2025-04-28 22:41:28 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,6 @@
+let num1 = parseInt (prompt(`enter the first number:`));
+let num2 = parseInt (prompt(`enter the second number:`));
+alert (`the sum of the given numbers is:${num1+num2}`);
+alert (`the difference of the given numbers is: ${num1-num2}`);
+alert (`the product of the given numbers is: ${num1*num2}`);
+alert (`the quotient of the given numbers is: ${num1/num2}`);
